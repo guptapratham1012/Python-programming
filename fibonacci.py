346d3840daed2f0770dffe7ea0a3604b0b3f9f07
guptapratham1012 <78781324+guptapratham1012@users.noreply.github.com>
2021-03-02 11:34:16 +0530

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,19 @@
+print('------------------------------------------')
+n = int(input('Enter the number of terms: '))
+n1=0
+n2=1
+count = 0
+if n <= 0:
+   print('Please enter a positive integer')
+elif n == 1:
+   print('Fibonacci sequence upto',n,'terms :')
+   print(n1)
+else:
+   print('Fibonacci sequence upto',n,'terms :')
+   while count < n:
+       print(n1)
+       nth = n1 + n2
+       n1 = n2
+       n2 = nth
+       count += 1
+print('\n------------------------------------------')
